25163cb231edaed68fa5e5f58295a5a338b04c25
Jsoctocat <42134925+jsoctocat@users.noreply.github.com>
2024-02-26 23:57:26 -0800

    Improving User Experience with Loading Skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -17,10 +17,13 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+
 
     useEffect (() => {
         const controller = new AbortController();
 
+        setLoading(true);
         fetch("https://api.rawg.io/api/games?key=0de0dc61e00d4d6f8c278a8c38c1e83e&dates=2019-09-01,2019-09-30&platforms=18,1,7", {
             signal: controller.signal,
             method: "GET",
@@ -32,12 +35,12 @@ const useGames = () => {
                 }
                 throw response;
             })
-            .then(data => { setGames(data.results); })
-            .catch(err => { if(err.status) setError("Request failed with status code " + err.status); })
+            .then(data => { setGames(data.results); setLoading(false); })
+            .catch(err => { if(err.status) setError("Request failed with status code " + err.status); setLoading(false); })
             return () => controller.abort();
     }, []);
     
-    return { games, error }
+    return { games, error, isLoading }
 }
 
 export default useGames

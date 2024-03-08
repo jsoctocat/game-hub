3550db961ee59008dcbec618da0d96423fecd901
Jsoctocat <42134925+jsoctocat@users.noreply.github.com>
2024-03-07 23:03:31 -0800

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../assets/360_F_552371867_LkVmqMEChRhMMHDQ2drOS8cwhAWehgVc.jpg'
+
 const GetCroppedImageUrl = (url: string) => {
-    if (!url) return '';
+    if (!url) return noImage;
 
     const target = 'media/';
     const index = url.indexOf(target) + target.length;

bb1ad764aa6aae5a9ee457e080663f05201d287a
Jsoctocat <42134925+jsoctocat@users.noreply.github.com>
2024-03-08 00:47:12 -0800

    Implement search

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,7 +22,9 @@ useData<Game>(
     { 
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
-        ordering: gameQuery.sortOrder },
+        ordering: gameQuery.sortOrder,
+        search: gameQuery.searchText,
+     },
     [gameQuery]);
 
 export default useGames
